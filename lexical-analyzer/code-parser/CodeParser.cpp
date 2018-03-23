5f24e1919c2d0e513fc599bd0a34dd0d026a3875
SalmaAhmed95 <salma19956@gmail.com>
2018-03-23 19:55:56 +0200

    matcher bug fixed

--- lexical-analyzer/code-parser/CodeParser.cpp
+++ lexical-analyzer/code-parser/CodeParser.cpp
@@ -8,20 +8,20 @@
 CodeParser::CodeParser(std::string fileName) { codeFile = fileName; }
 
 std::vector<char> CodeParser::parseFile() {
-  std::ifstream file(codeFile);
-  std::string temp;
-  char c;
-  while (file.get(c)) { // loop getting single characters
-    chars.push_back(c);
-  }
-  file.close();
-  return chars;
+    std::ifstream file(codeFile);
+    std::string temp;
+    char c;
+    while (file.get(c)) { // loop getting single characters
+        chars.push_back(c);
+    }
+    file.close();
+    return chars;
 }
 
 char CodeParser::getChar() {
-  char c = chars[index];
-  index++;
-  return c;
+    char c = chars[index];
+    index++;
+    return c;
 }
 
 void CodeParser::setStartIndex(int i) { index = i; }
@@ -29,27 +29,27 @@ void CodeParser::setStartIndex(int i) { index = i; }
 int CodeParser::getCurIndex() { return index; }
 
 std::string CodeParser::getSubString(int startIndex, int lastIndex) {
-  std::string subString;
-  for (int i = startIndex; i < lastIndex; i++) {
-    subString += chars[i];
-  }
-  return subString;
+    std::string subString;
+    for (int i = startIndex; i < lastIndex; i++) {
+        subString += chars[i];
+    }
+    return subString;
 }
 
 bool CodeParser::hasChars() {
-  if (index == chars.size() - 1)
-    return false;
-  return true;
+    if (index == chars.size())
+        return false;
+    return true;
 }
 
 bool CodeParser::isDelimeter(char c) { return iswspace(c); }
 
 std::string CodeParser::getIdentifierClass(std::string propertiesFileName) {
-  Properties::PropertiesData properties;
-  std::ifstream propertiesFile(propertiesFileName.c_str());
-  propertiesFile >> properties;
-  if (properties.find(IDENTIFIER) != properties.end()) {
-    return properties.find(IDENTIFIER)->second;
-  }
-  return IDENTIFIER;
+    Properties::PropertiesData properties;
+    std::ifstream propertiesFile(propertiesFileName.c_str());
+    propertiesFile >> properties;
+    if (properties.find(IDENTIFIER) != properties.end()) {
+        return properties.find(IDENTIFIER)->second;
+    }
+    return IDENTIFIER;
 }
