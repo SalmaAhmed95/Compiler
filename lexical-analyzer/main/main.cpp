624ed0ec9b8c03928ce473cf1f22e4ddb8ff9718
moamen1996 <moamenlebaroudy.me@gmail.com>
2018-03-23 23:03:21 +0200

    Testing and passing args to main.

--- lexical-analyzer/main/main.cpp
+++ lexical-analyzer/main/main.cpp
@@ -9,35 +9,61 @@
 #include "../dfa-minimizer/DfaMinimizer.h"
 #include "../pattern-matcher/PatternMatcher.h"
 
+#define FILES_NUM 4
+
+const std::string DEFAULT_PROPERTIES = "properties.ini";
+const std::string DEFAULT_LEXICAL = "lexical_rules.txt";
+const std::string DEFAULT_CODE = "code.txt";
+
 void deleteTokens(std::vector<Token *> v) {
-    for (int i = 0; i < v.size(); i++) {
-        delete v[i];
-    }
+  for (int i = 0; i < v.size(); i++) {
+    delete v[i];
+  }
 }
 
-int main() {
-    std::vector<Token *> tokens =
-            ProductionParser::loadLexicalRules("lexical_rules.txt", "properties.ini");
-    NFA *nfa = RegexToNfaConverter::getNfa(tokens);
-    deleteTokens(tokens);
-    std::cout << "Finished NFA" << std::endl;
-    DFA *dfa = NfaToDfaConverter::getDFA(nfa);
-    std::cout << "Finished DFA \n";
-    DFA *dfaMin = DfaMinimizer::getInstance().minimizeDfa(dfa);
-    std::cout << "Finished Minimization\n";
-    // TODO call non default constructor if main has paramters for output file
-    // else default constructor
-    FileWriter *writer = new FileWriter();
-    writer->writeTransitionTable(dfaMin);
-    PatternMatcher *matcher =
-            new PatternMatcher(dfaMin, "code.txt", "properties.ini", writer);
-    matcher->analyzeCode();
-    writer->closeFile();
-    std::cout << "Finished Matching\n";
-    delete nfa;
-    delete dfa;
-    delete dfaMin;
-    delete matcher;
-    delete writer;
-    return 0;
+void getFileNames(int argc, char **argv, std::string &lexicalRules,
+                  std::string &properties, std::string &code,
+                  std::string &output) {
+  std::cout << argc << '\n';
+  if (argc != FILES_NUM + 1) {
+    lexicalRules = DEFAULT_LEXICAL;
+    properties = DEFAULT_PROPERTIES;
+    code = DEFAULT_CODE;
+    output = DEFAULT_OUTPUT;
+  } else {
+    std::cout << "/* message */" << '\n';
+    lexicalRules = argv[1];
+    properties = argv[2];
+    code = argv[3];
+    output = argv[4];
+  }
+}
+
+int main(int argc, char **argv) {
+  std::string lexicalRules, code, properties, output;
+  getFileNames(argc, argv, lexicalRules, properties, code, output);
+  std::vector<Token *> tokens =
+      ProductionParser::loadLexicalRules(lexicalRules, properties);
+  NFA *nfa = RegexToNfaConverter::getNfa(tokens);
+  deleteTokens(tokens);
+  std::cout << "Finished NFA" << std::endl;
+  DFA *dfa = NfaToDfaConverter::getDFA(nfa);
+  std::cout << "Finished DFA \n";
+  DFA *dfaMin = DfaMinimizer::getInstance().minimizeDfa(dfa);
+  std::cout << "Finished Minimization\n";
+  // TODO call non default constructor if main has paramters for output file
+  // else default constructor
+  FileWriter *writer = new FileWriter(output);
+  writer->writeTransitionTable(dfaMin);
+  PatternMatcher *matcher =
+      new PatternMatcher(dfaMin, code, properties, writer);
+  matcher->analyzeCode();
+  writer->closeFile();
+  std::cout << "Finished Matching\n";
+  delete nfa;
+  delete dfa;
+  delete dfaMin;
+  delete matcher;
+  delete writer;
+  return 0;
 }
